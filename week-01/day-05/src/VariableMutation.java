dd9fbf5f4751fc88e900c6f6ea33825b95bbf248
Hajnóczky Viktor <ravenrot@gmail.com>
2021-04-09 17:59:03 +0200

    week-1-day-5-exercises

---
+++ week-01/day-05/src/VariableMutation.java
@@ -0,0 +1,47 @@
+public class VariableMutation {
+    public static void main(String[] args) {
+
+        // make the "a" variable's value bigger by 10
+        int a = 3;
+        a += 10;
+        System.out.println(a);
+
+        // make b smaller by 7
+        int b = 100;
+        b -= 7;
+        System.out.println(b);
+
+        // please double c's value
+        int c = 44;
+        c *= 2;
+        System.out.println(c);
+
+        // please divide by 5 d's value
+        int d = 125;
+        d /= 5;
+        System.out.println(d);
+
+        // please cube of e's value
+        int e = 8;
+        e ^= 8;
+        System.out.println(e);
+
+        int f1 = 123;
+        int f2 = 345;
+        // tell if f1 is bigger than f2 (print as a boolean)
+
+        int g1 = 350;
+        int g2 = 200;
+        // tell if the double of g2 is bigger than g1 (print as a boolean)
+
+        int h = 135798745;
+        // tell if it has 11 as a divisor (print as a boolean)
+
+        int i1 = 10;
+        int i2 = 3;
+        // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
+
+        int j = 1521;
+        // tell if j is dividable by 3 or 5 (print as a boolean)
+    }
+}
