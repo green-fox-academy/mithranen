475f8e5a75b2a2619a54303c546c363f35b34c17
Hajnóczky Viktor <ravenrot@gmail.com>
2021-04-23 00:58:04 +0200

    week03-day03 second commit

--- week-01/src/Loops/GuessTheNumber.java
+++ week-01/src/Loops/GuessTheNumber.java
@@ -1,5 +1,7 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class GuessTheNumber {
 
   // Write a program that stores a number and the user has to figure it out
@@ -11,6 +13,21 @@ public class GuessTheNumber {
 // You found the number: 8
 
   public static void main(String[] args) {
-
+    int storedNumber = 8;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Guess the number! Give your tip: ");
+    int userInput = sc.nextInt();
+    while (userInput != storedNumber) {
+      if (userInput > storedNumber) {
+        System.out.println("The stored number is lower. Try again: ");
+        //userInput = sc.nextInt(); felesleges, kiemelhető while végére
+      }
+      if (userInput < storedNumber) {
+        System.out.println("The stored number is higher. Try again: ");
+        //userInput = sc.nextInt();
+      }
+      userInput = sc.nextInt();
+    }
+    System.out.println("Congrats, eat your shirt!");
   }
 }
